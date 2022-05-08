c9902fa5a498ffe71e211e88bc8885e113d62d30
unknown <hariharanhighlevelprogrammer.com>
2022-05-09 00:43:33 +0530

    Scommit

--- job_portal/job_portal/settings.py
+++ job_portal/job_portal/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 import os
+from django import django_heroku
+#import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,3 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+STATIC_ROOT =os.path.join(BASE_DIR,'static')
+
+django_heroku.settings(locals())
